d777a5a5209936095fc81668cc8e4ada9f7d48da
Savas Gioldasis s.gioldasis@gmail.com <s.gioldasis@gmail.com>
2020-03-23 18:33:56 +0200

    Add INSERT statement

--- kafkamysql/app.py
+++ kafkamysql/app.py
@@ -22,36 +22,49 @@ class KafkaMySql:
     @staticmethod
     def process(a_string):
         try:
-            # a_json = json.loads(a_string)
-            # logging.info(a_json)
+            a_json = json.loads(a_string)
+            logging.info(a_json)
             # df = pd.DataFrame.from_dict(a_json, orient="index")
-            # df = pd.DataFrame.from_dict([a_json])
-            df = pd.read_json('['+a_string+']', orient='columns') 
-            df['calc'] = df.apply(lambda row: row.created_at.value, axis=1)
+            df = pd.DataFrame.from_dict([a_json])
+            # df = pd.read_json('['+a_string+']', orient='columns') 
+
+            # df['calc'] = df['created_at'].values.astype(str)
+            calc = df.apply(lambda row: pd.to_datetime(row.created_at), axis=1)
+            df['created_at_ts'] = calc.values.astype('int64')
+
             # df['sqlts'] = df.apply(lambda row: row.created_at.value.dt.round('1U'), axis=1)
-            df['sqlts'] = df['created_at'].values.astype('datetime64[us]')
-            df['sqlts2'] = df['created_at'].dt.round('1U').values.astype('datetime64[us]')
-            df['sqlts3'] = df['created_at'].dt.round('1U').values.astype(str)
+            # df['sqlts'] = df['created_at'].values.astype('datetime64[us]')
+            # df['sqlts2'] = df['created_at'].dt.round('1U').values.astype('datetime64[us]')
+            # df['sqlts3'] = df['created_at'].dt.round('1U').values.astype(str)
 
             logging.info(df)
             logging.info(df.dtypes)
 
-            logging.info(df['sqlts'].astype(str).tolist())
-            logging.info(df['sqlts2'].astype(str).tolist())
-            logging.info(df['sqlts3'].astype(str).tolist())
+            # logging.info(df['created_at'].astype(str).tolist())
+            # logging.info(df['created_at'].dt.tolist())
+            # logging.info(df['created_at'].view('int64').tolist())
 
-            logging.info(df['created_at'].astype(str).tolist())
-            logging.info(df['created_at'].dt.round('1U').astype(str).tolist())
-            logging.info(df['created_at'].dt.round('1U').tolist())
-            logging.info(df['created_at'].dt.strftime('%Y-%m-%d %H:%M:%S.%f').tolist())
-            logging.info(df['created_at'].view('int64').tolist())
+            # logging.info(df['sqlts'].astype(str).tolist())
+            # logging.info(df['sqlts2'].astype(str).tolist())
+            # logging.info(df['sqlts3'].astype(str).tolist())
+
+            # logging.info(df['created_at'].dt.round('1U').astype(str).tolist())
+            # logging.info(df['created_at'].dt.round('1U').tolist())
+            # logging.info(df['created_at'].dt.strftime('%Y-%m-%d %H:%M:%S.%f').tolist())
 
             # creating column list for insertion
-            # cols = "`,`".join([str(i) for i in df.columns.tolist()])
-            # for i,row in df.iterrows():
-            #     logging.warning(f"row[{i}] = "+str(type(row)))
+            cols = "`,`".join([str(i) for i in df.columns.tolist()])
+            logging.info(cols)
+
+            # Insert DataFrame recrds one by one.
+            for i,row in df.iterrows():
+                sql = "INSERT INTO `Classifieds` (`" +cols + "`) VALUES (" + "%s,"*(len(row)-1) + "%s)"
+                logging.info(sql)
+                logging.info(tuple(row))
+
         except:
             logging.warning(f"String {a_string} could not be converted to JSON")
+            
         finally:
             return a_string
 
