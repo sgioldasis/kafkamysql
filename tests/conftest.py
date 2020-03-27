911016b289a0a8a8f1f40c6fa10fd6148a726342
Savas Gioldasis s.gioldasis@gmail.com <s.gioldasis@gmail.com>
2020-03-27 13:49:46 +0200

    Improved tests and cleanup

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,11 +1,102 @@
 import logging
 import pytest
+import os
+from .context import utils
+from confluent_kafka import Producer
+from confluent_kafka.admin import (
+    AdminClient,
+    NewTopic,
+    NewPartitions,
+    ConfigResource,
+    ConfigSource,
+)
 
 LOGGER = logging.getLogger(__name__)
 
 
-@pytest.fixture(scope='function')
-def example_fixture():
-    LOGGER.info("Setting Up Example Fixture...")
-    yield
-    LOGGER.info("Tearing Down Example Fixture...")
+def kafka_create_topics(a, topics):
+    """ Create topics """
+
+    new_topics = [
+        NewTopic(topic, num_partitions=1, replication_factor=1) for topic in topics
+    ]
+    # Call create_topics to asynchronously create topics, a dict
+    # of <topic,future> is returned.
+    fs = a.create_topics(new_topics)
+
+    # Wait for operation to finish.
+    # Timeouts are preferably controlled by passing request_timeout=15.0
+    # to the create_topics() call.
+    # All futures will finish at the same time.
+    for topic, f in fs.items():
+        try:
+            f.result()  # The result itself is None
+            logging.warning("Topic {} created".format(topic))
+        except Exception as e:
+            logging.warning("Failed to create topic {}: {}".format(topic, e))
+
+
+def kafka_delete_topics(a, topics):
+    """ delete topics """
+
+    # Call delete_topics to asynchronously delete topics, a future is returned.
+    # By default this operation on the broker returns immediately while
+    # topics are deleted in the background. But here we give it some time (30s)
+    # to propagate in the cluster before returning.
+    #
+    # Returns a dict of <topic,future>.
+    fs = a.delete_topics(topics, operation_timeout=30)
+
+    # Wait for operation to finish.
+    for topic, f in fs.items():
+        try:
+            f.result()  # The result itself is None
+            print("Topic {} deleted".format(topic))
+        except Exception as e:
+            print("Failed to delete topic {}: {}".format(topic, e))
+
+
+@pytest.fixture(scope="session")
+def env():
+    return os.getenv("TEST_ENV")
+
+
+@pytest.fixture(scope="session")
+def config(env):
+    return utils.load_config(env)
+
+
+@pytest.fixture(scope="session")
+def db_connection(config):
+    mysql_config = config["mysql"]
+    mysql_table = mysql_config["table"]
+    connection = utils.connect(mysql_config)
+    yield connection
+    connection.close()
+
+
+@pytest.fixture(scope="function")
+def mysql_table(config, db_connection):
+    db_cursor = db_connection.cursor()
+    db_cursor.execute(f"delete from `{config['mysql']['table']}`")
+    db_connection.commit()
+    db_cursor.close()
+    return config["mysql"]["table"]
+
+
+@pytest.fixture(scope="session")
+def kafka_admin_client(config):
+    return AdminClient({"bootstrap.servers": config["kafka"]["broker_url"]})
+
+
+@pytest.fixture(scope="session")
+def kafka_producer(config):
+    return Producer({"bootstrap.servers": config["kafka"]["broker_url"]})
+
+
+@pytest.fixture(scope="function")
+def kafka_topic(config, kafka_admin_client):
+    kafka_topic = config["kafka"]["topic"]
+    kafka_create_topics(kafka_admin_client, [kafka_topic])
+    yield kafka_topic
+    kafka_delete_topics(kafka_admin_client, [kafka_topic])
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,47 +1,20 @@
-import os
 from .context import kafkamysql
-from .context import utils
-from confluent_kafka import Producer
-from confluent_kafka.admin import (
-    AdminClient,
-    NewTopic,
-    NewPartitions,
-    ConfigResource,
-    ConfigSource,
-)
-import logging
+import pytest
 import json
 import time
-
-KAFKA_BROKER_URL = os.getenv("KAFKA_BROKER_URL")
-KAFKA_TOPIC = "data"
-
-
-def example_create_topics(a, topics):
-    """ Create topics """
-
-    new_topics = [
-        NewTopic(topic, num_partitions=1, replication_factor=1) for topic in topics
-    ]
-    # Call create_topics to asynchronously create topics, a dict
-    # of <topic,future> is returned.
-    fs = a.create_topics(new_topics)
-
-    # Wait for operation to finish.
-    # Timeouts are preferably controlled by passing request_timeout=15.0
-    # to the create_topics() call.
-    # All futures will finish at the same time.
-    for topic, f in fs.items():
-        try:
-            f.result()  # The result itself is None
-            logging.warning("Topic {} created".format(topic))
-        except Exception as e:
-            logging.warning("Failed to create topic {}: {}".format(topic, e))
+from decimal import Decimal
+import datetime
 
 
-def example_data():
+@pytest.fixture(scope="function")
+def input_id():
+    return "d6707ce40a1447baaf012f948fb5b356"
+
+
+@pytest.fixture(scope="function")
+def input_data(input_id):
     data = {
-        "id": "d6707ce40a1447baaf012f948fb5b356",
+        "id": input_id,
         "customer_id": "8e0fdcefc3ad45acbb5a2abc506c6c9f",
         "created_at": "2019-05-04T23:34:19.5934998Z",
         "text": "<p>aliquam sed amet dolore consectetuer ut dolore elit dolor euismod elit erat</p>",
@@ -54,69 +27,12 @@ def example_data():
     json_str = json.dumps(data)
     return json_str
 
-def example_data2():
-    data = {
-        "id": "e",
-        "customer_id": "8e0fdcefc3ad45acbb5a2abc506c6c9f",
-        "created_at": "2019-05-04T23:34:19.5934998Z",
-        "text": "<p>aliquam sed amet dolore consectetuer ut dolore elit dolor euismod elit erat</p>",
-        "ad_type": "Premium",
-        "price": 29.04,
-        "currency": "EUR",
-        "payment_type": "Card",
-        "payment_cost": 0.17,
-    }
-    json_str = json.dumps(data)
-    return json_str
 
-def test_app(capsys, example_fixture):
-
-    # env = "test"
-    env = os.getenv("TEST_ENV")
-    config = utils.load_config(env)
-
-
-    # Connect to database and get a cursor
-    mysql_config = config["mysql"]
-    db_connection = utils.connect(mysql_config)
-    mysql_table = mysql_config["table"]
-
-    id = "d6707ce40a1447baaf012f948fb5b356"
-    db_cursor = db_connection.cursor()
-    db_cursor.execute(f"delete from `{mysql_table}`")
-    db_connection.commit()
-    db_cursor.close()
-
-    # Create Admin client
-    kafka_broker_url = config["kafka"]["broker_url"]
-    kafka_topic = config["kafka"]["topic"]
-    a = AdminClient({"bootstrap.servers": kafka_broker_url})
-    example_create_topics(a, [kafka_topic])
-
-    producer = Producer({"bootstrap.servers": kafka_broker_url})
-    producer.produce(KAFKA_TOPIC, key="1", value="Hello, World!")
-    producer.produce(KAFKA_TOPIC, key="2", value=example_data())
-    producer.produce(KAFKA_TOPIC, key="3", value=example_data2())
-    producer.produce(KAFKA_TOPIC, key="4", value="Quit!")
-    producer.flush(10)
-
-    # pylint: disable=W0612,W0613
-    kafkamysql.KafkaMySql.run(env)
-    captured = capsys.readouterr()
-
-    # assert "Hello, World" in captured.out
-    # assert "d6707ce40a1447baaf012f948fb5b356" in captured.out
-
-    # Check fields
-    from decimal import Decimal
-    import datetime
-    db_cursor = db_connection.cursor()
-    db_cursor.execute(f"select * from `{mysql_table}` where id='{id}'")
-    result = db_cursor.fetchall()
-    db_cursor.close()
-    assert result == [
+@pytest.fixture(scope="function")
+def output_data(input_id):
+    return [
         (
-            "d6707ce40a1447baaf012f948fb5b356",
+            input_id,
             "8e0fdcefc3ad45acbb5a2abc506c6c9f",
             "2019-05-04T23:34:19.5934998Z",
             "<p>aliquam sed amet dolore consectetuer ut dolore elit dolor euismod elit erat</p>",
@@ -130,8 +46,39 @@ def test_app(capsys, example_fixture):
         )
     ]
 
-    # Close the cursor
-    db_cursor.close()
 
-    # Close the connection to database
-    db_connection.close()
+# Get record with specific id from database
+def db_get_id(db_connection, mysql_table, input_id):
+    db_cursor = db_connection.cursor()
+    db_cursor.execute(f"select * from `{mysql_table}` where id='{input_id}'")
+    result = db_cursor.fetchall()
+    db_cursor.close()
+    return result
+
+
+def test_app(
+    env,
+    kafka_producer,
+    kafka_topic,
+    input_data,
+    db_connection,
+    mysql_table,
+    input_id,
+    output_data,
+    capsys,
+    caplog,
+):
+
+    # Write input data to Kafka topic
+    kafka_producer.produce(kafka_topic, key="1", value=input_data)
+    kafka_producer.produce(kafka_topic, key="2", value=input_data)
+    kafka_producer.produce(kafka_topic, key="3", value="Quit!")
+    kafka_producer.flush()
+
+    # pylint: disable=W0612,W0613
+    kafkamysql.KafkaMySql.run(env)
+    captured = capsys.readouterr()
+
+    # Check output data
+    assert db_get_id(db_connection, mysql_table, input_id) == output_data
+    assert 'Offending data: \nQuit!' in caplog.text
